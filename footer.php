d34c14f274b6b27a41940b564ab27d2960dc97a0
westonh2 <weston@full-circle.design>
2023-04-24 12:36:56 -0400

    footer rearrange

--- footer.php
+++ footer.php
@@ -24,13 +24,13 @@
     <li><a href="https://www.burnsscalorealestate.com/about/">ABOUT</a></li>
     <li><a href="https://www.burnsscalorealestate.com/media/">MEDIA</a></li>
     <li class="divider d-md-none"></li>
-    <li class="#"><a href="https://www.burnsscalorealestate.com/career-hub/">CAREER HUB</a></li>
     <li class="d-lg-none"><a href="https://www.burnsscalorealestate.com/privacy/">PRIVACY POLICY</a></li>
     <li class="d-lg-none"><a href="https://www.burnsscalorealestate.com/accessibility/">ACCESSIBILITY</a></li>
     <li class="divider d-md-none"></li>
-    <li class="d-block"><a href="https://www.securecafe3.com/tenantportal/commercialleasing/userlogin.aspx?companyid=1" target="_blank">CLIENT PORTAL</a></li>
-    <li class="d-block d-lg-none"><a href="<?php echo get_site_url(); ?>">PARTNER PERKS</a></li>
+    <li class="#"><a href="https://www.burnsscalorealestate.com/career-hub/">CAREER HUB</a></li>
+    <li class="d-block"><a href="<?php echo get_site_url(); ?>">PARTNER PERKS</a></li>
     <li class="divider d-md-none"></li>
+    <li class="d-block"><a href="https://www.securecafe3.com/tenantportal/commercialleasing/userlogin.aspx?companyid=1" target="_blank">CLIENT PORTAL</a></li>
     <li class="d-lg-none"><a href="https://www.burnsscalorealestate.com/company-portal/" target="_blank">EMPLOYEE PORTAL</a></li>
     <li class="d-none d-lg-block"><button class="cursor-pointer button-remove-style" onclick="footerExpand(), footerList()">MORE^</button></li>
   </ul>
